94a81212b07eea1e80012eeb7b56d2c78f36252c
Alex Lehner <alexlehner314@gmail.com>
2022-07-19 21:53:42 -0400

    Format

--- scheduler.hh
+++ scheduler.hh
@@ -14,25 +14,25 @@ using Minisat::lbool;
 
 class Scheduler {
 
-  private:
-    unordered_set<CourseOfferings> course_offerings_;
-    unordered_set<TimeConstraint> time_constraints_;
-    unordered_set<ChunkConstraint> chunk_constraints_;
-    unordered_set<CutOffTimeConstraint> cut_off_time_constraints_;
+    private:
+        unordered_set<CourseOfferings> course_offerings_;
+        unordered_set<TimeConstraint> time_constraints_;
+        unordered_set<ChunkConstraint> chunk_constraints_;
+        unordered_set<CutOffTimeConstraint> cut_off_time_constraints_;
 
-  public:
-   Scheduler();
+    public:
+        Scheduler();
 
-    // Setting Data 
-   void add_course(CourseOfferings course);
-   void remove_course(CourseOfferings course);
+        // Setting Data 
+        void add_course(CourseOfferings course);
+        void remove_course(CourseOfferings course);
 
-    // Accessors 
-    // get highest priority constraints etc 
+        // Accessors 
+        // get highest priority constraints etc 
 
-    // MINISAT Solver Functions 
-   void Solver::init_variables()
-   bool Solver::apply_board(board const& b)
-   void Solver::exactly_one_true(Minisat::vec<Minisat::Lit> const& literals)
+        // MINISAT Solver Functions 
+        void Solver::init_variables()
+        bool Solver::apply_board(board const& b)
+        void Solver::exactly_one_true(Minisat::vec<Minisat::Lit> const& literals)
 };
 
