d47854c1b48e8850d3024af87bbc8e904193618f
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-07-19 20:28:03 -0400

    Added comment

---
+++ scheduler.cc
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+/*
+
+set up each course as one part of Boolean equation
+ (x1 | -x5 | x4) & #all lecture sections
+  (-x1 | x5 | x3 | x4) & # all practical sections
+  (-x3 | x4). #all tutorial sections
+       #pick one lecture section     #one pra section   # one tut section
+ (   (lec101 | lec102 | lec103) & (pra101 | pra102) & (tut101 | tut102 | tut103)  )
+*/
