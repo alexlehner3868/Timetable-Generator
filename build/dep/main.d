21d9833f188a26a3aa4de9b37cea3bc175677237
Alex <angrybird3868@gmail.com>
2022-08-22 12:58:05 -0400

    Remove .o fikes

--- build/dep/main.d
+++
@@ -1,2 +0,0 @@
-./build/obj/.o: src/main.cc include/constraints.hh include/scheduler.hh \
-  include/course_offering.hh include/section.hh include/period.hh
--- build/dep/scheduler.d
+++
@@ -1,3 +0,0 @@
-./build/obj/scheduler.o: src/scheduler.cc include/period.hh \
-  include/scheduler.hh include/course_offering.hh include/section.hh \
-  include/constraints.hh
