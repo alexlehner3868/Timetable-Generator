3a6d46dc253fa16cc8d7424b223b7283b4981bb6
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-08-31 13:09:29 -0400

    Added examples to main

--- src/main.cc
+++ src/main.cc
@@ -104,6 +104,147 @@ void test_one_class_multiple_sections(){
     Scheduler scheduler;
     scheduler.schedule_classes(offerings);   
 }
+void test_two_classes_one_section() {
+    vector<Section> calc_lecture_sections;
+    vector<Section> linalg_lecture_sections;
+    vector<Section> empty_vec;
+    vector<int> math_durations;
+    vector<int> math_start_time;
+    vector<int> math_day;
+    vector<char> math_semester;
+    vector<bool> math_async;
+
+
+    // Calculus
+    // -- Section 1 
+    // Monday 9 - 10
+    // Wednesday 10 - 12
+    // Thursday 1 - 2
+    math_durations.insert(calc_durations.end(), { 1, 2, 1 });
+    math_start_time.insert(calc_start_time.end(), { 9, 10, 13 });
+    math_day.insert(calc_day.end(), { 1, 3, 4 });
+    math_semester.insert(calc_semester.end(), { 'F', 'F', 'F' });
+    math_async.insert(calc_async.end(), { false, false, false });
+
+    Section calc_section_1(1, math_durations, math_start_time, math_semester, math_day, math_async);
+    calc_lecture_sections.push_back(calc_section_1);
+
+
+    // Linear Algebra
+    // -- Section 1
+    // Tuesday 9 - 10
+    // Wednesday 1 - 2
+    // Friday 11 - 12
+    math_durations.clear();
+    math_start_time.clear();
+    math_day.clear();
+    math_semester.clear();
+    math_async.clear();
+    math_durations.insert(calc_durations.end(), { 1, 1, 1 });
+    math_start_time.insert(calc_start_time.end(), { 9, 13, 11 });
+    math_day.insert(calc_day.end(), { 2, 3, 5 });
+    math_semester.insert(calc_semester.end(), { 'F', 'F', 'F' });
+    math_async.insert(calc_async.end(), { false, false, false });
+
+    Section linalg_section_1(1, math_durations, math_start_time, math_semester, math_day, math_async);
+    linalg_lecture_sections.push_back(linalg_section_1);
+
+
+    CourseOfferings calculus("Calculus 1", "MAT186", calc_lecture_sections, empty_vec, empty_vec);
+    CourseOfferings linear_algebra("Linear Algebra", "MAT188", linalg_lecture_sections, empty_vec, empty_vec);
+
+    unordered_set<CourseOfferings, CourseOfferings::CourseOfferingHash> offerings;
+    offerings.insert(calculus);
+    offerings.insert(linear_algebra);
+    Scheduler scheduler;
+    scheduler.schedule_classes(offerings);
+}
+
+
+
+void test_two_classes_two_sections() {
+    vector<Section> calc_lecture_sections;
+    vector<Section> linalg_lecture_sections;
+    vector<Section> empty_vec;
+    vector<int> math_durations;
+    vector<int> math_start_time;
+    vector<int> math_day;
+    vector<char> math_semester;
+    vector<bool> math_async;
+
+
+    // Calculus
+    // -- Section 1 
+    // Monday 9 - 10
+    // Wednesday 10 - 12
+    // Thursday 1 - 2
+    math_durations.insert(calc_durations.end(), { 1, 2, 1 });
+    math_start_time.insert(calc_start_time.end(), { 9, 10, 13 });
+    math_day.insert(calc_day.end(), { 1, 3, 4 });
+    math_semester.insert(calc_semester.end(), { 'F', 'F', 'F' });
+    math_async.insert(calc_async.end(), { false, false, false });
+
+    Section calc_section_1(1, math_durations, math_start_time, math_semester, math_day, math_async);
+    calc_lecture_sections.push_back(calc_section_1);
+
+
+    // -- Section 2
+    // Monday 10 - 11
+    // Tuesday 10 - 11
+    // Thursday 10 - 11
+
+    Section calc_section_1(2, math_durations, math_start_time, math_semester, math_day, math_async);
+    calc_lecture_sections.push_back(calc_section_1);
+
+    // Linear Algebra
+    // -- Section 1
+    // Tuesday 9 - 10
+    // Wednesday 1 - 2
+    // Friday 11 - 12
+    math_durations.clear();
+    math_start_time.clear();
+    math_day.clear();
+    math_semester.clear();
+    math_async.clear();
+    math_durations.insert(calc_durations.end(), { 1, 1, 1 });
+    math_start_time.insert(calc_start_time.end(), { 9, 13, 11 });
+    math_day.insert(calc_day.end(), { 2, 3, 5 });
+    math_semester.insert(calc_semester.end(), { 'F', 'F', 'F' });
+    math_async.insert(calc_async.end(), { false, false, false });
+
+    Section linalg_section_1(1, math_durations, math_start_time, math_semester, math_day, math_async);
+    linalg_lecture_sections.push_back(linalg_section_1);
+
+
+    // -- Section 2
+    // Tuesday 4 - 5
+    // Wednesday 4 - 5
+    // Friday 4 - 5
+
+    Section linalg_section_1(2, math_durations, math_start_time, math_semester, math_day, math_async);
+    linalg_lecture_sections.push_back(linalg_section_1);
+
+    CourseOfferings calculus("Calculus 1", "MAT186", calc_lecture_sections, empty_vec, empty_vec);
+    CourseOfferings linear_algebra("Linear Algebra", "MAT188", linalg_lecture_sections, empty_vec, empty_vec);
+
+    unordered_set<CourseOfferings, CourseOfferings::CourseOfferingHash> offerings;
+    offerings.insert(calculus);
+    offerings.insert(linear_algebra);
+    Scheduler scheduler;
+    scheduler.schedule_classes(offerings);
+}
+void test_two_classes_multiple_sections() {
+
+}
+void test_three_classes_multiple_sections() {
+
+}
+void test_two_classes_conflict() {
+
+}
+void test_three_classes_conflict() {
+
+}
 
 
 void test_sample_questions(){
@@ -113,11 +254,24 @@ void test_sample_questions(){
     // One class with multiple sections 
     cout<<"Test 2: One Class, Multiple Sections "<<endl;
     test_one_class_multiple_sections();
-
-    {
-        //Test 3: Mat186 Lec1  (no conflicts )
-        //        LinAlg Lec1  
-    }
+    // Two classes with one section each
+    cout << "Test 3: Two Classes, One Section " << endl;
+    test_two_classes_one_section();
+    // Two classes with two sections
+    cout << "Test 4: Two Classes, Two Sections " << endl;
+    test_two_classes_two_sections();
+    // Two classes with two sections each
+    cout << "Test 5: Two Classes, Multiple Sections " << endl;
+    test_two_classes_multiple_sections();
+    // Three classes with two sections each
+    cout << "Test 6: Three Classes, Multiple Sections " << endl;
+    test_three_classes_multiple_sections();
+    // Two classes with two sections and a conflict 
+    cout << "Test 7: Two Classes, Two Sections, Conflict " << endl;
+    test_two_classes_conflict();
+    // Three classes with multiple sections and conflicts
+    cout << "Test 8: Three Classes, Multiple Sections, Conflicts" << endl;
+    test_three_classes_conflict();
 
     {
         //Test 4: Mat186 Lec1 Lec2 (no conflicts )
