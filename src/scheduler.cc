22f83a89e82f7c3a6add72028a04d257e392eca1
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-09-09 20:31:34 -0400

    Fixed compilation errors

--- src/scheduler.cc
+++ src/scheduler.cc
@@ -46,7 +46,6 @@ void Scheduler::schedule_classes_helper(unordered_set<CourseOfferings, CourseOff
   // Loop through all of the Course Offerings (ie the course and all its sections)
   for(auto course : courses){
     //std::cout<<"TRYING TO ADD LEC "<< course.courseID()<<endl;
-    schedule_lectures
     attempt_to_add_section(timetable, LEC, course, courses);
     //std::cout<<"TRYING TO ADD TUT "<< course.courseID()<<endl;
     //attempt_to_add_section(timetable, TUT, course, courses);
@@ -61,45 +60,37 @@ void Scheduler::schedule_classes_helper(unordered_set<CourseOfferings, CourseOff
 
 void Scheduler::attempt_to_add_section(std::unordered_map<Date, SelectedCourseSection, Date_Hash>& timetable, int class_type, CourseOfferings course, unordered_set<CourseOfferings, CourseOfferings::CourseOfferingHash>& courses){
     int num_sections = 0;
-    Section section;
-    // Loop through all of the possible lecture sections in the course 
-    for (int type_of_class = 0; type_of_class < 3; type_of_class++) {
-        if(type_of_class == 0){
-            num_sections = (int) course.numLecSections();
-        }else if(type_of_class == 1){
+    if(class_type == LEC){
+          num_sections = (int) course.numLecSections();
+    }else if(class_type == TUT){
           num_sections = (int) course.numTutSections();
+    }else{
+      num_sections = (int) course.numPraSections();
+    }
+    
+    // Loop through all of the possible lecture sections in the course 
+    for(int section_id = 0; section_id < num_sections; section_id++){
+      //cout<<"Looking at section id "<<section_id<<endl;
+        Section section;
+        if(class_type == LEC){
+          section = course.lecture_sections_.at(section_id);
+        }else if(class_type == TUT){
+          section = course.tutorial_sections_.at(section_id);
         }else{
-          num_sections = (int) course.numPraSections();
+          section = course.practical_sections_.at(section_id);
         }
-        
-        
-        
-        
-        
-        
-        for(int section_id = 0; section_id < num_sections; section_id++){
-          //cout<<"Looking at section id "<<section_id<<endl;
-            
-            if(type_of_class == 0){
-            section = course.lecture_sections_.at(section_id);
-            }else if(type_of_class == 1){
-              section = course.tutorial_sections_.at(section_id);
-            }else{
-              section = course.practical_sections_.at(section_id);
-            }
-            
-            int class_in_section;
-            // Create an object to represent the section that was chosen 
-            SelectedCourseSection class_chosen{
-              .course_code = course.course_id_,
-              .type = class_type, // Lecture
-              .section = section_id,
-              .semester = section.semester_.at(0) // Each section should only be in either F or W (need support for full year courses)
-              // semester can be a char instead of a vector
-              // F - FALL W - WINTER B - BOTH
-            };
-          bool successfully_inserted;
-          Date period;
+        int class_in_section;
+        // Create an object to represent the section that was chosen 
+        SelectedCourseSection class_chosen{
+          .course_code = course.course_id_,
+          .type = class_type, // Lecture
+          .section = section_id,
+          .semester = section.semester_.at(0) // Each section should only be in either F or W (need support for full year courses)
+          // semester can be a char instead of a vector
+          // F - FALL W - WINTER B - BOTH
+        };
+      bool successfully_inserted;
+      Date period;
           // Try adding all of the lecture sections for that section and class to the timetable 
           for (class_in_section = 0; class_in_section < (int)section.duration_.size(); class_in_section++) {
               // Add a entry for every hour that the lecure has
@@ -157,7 +148,7 @@ void Scheduler::attempt_to_add_section(std::unordered_map<Date, SelectedCourseSe
                 }
         }
       }
-    }
+  
 }
 
 int Scheduler::max_sections_scheduled(){
@@ -172,7 +163,7 @@ void Scheduler::print_timetables(){
   int max_scheduled = max_sections_scheduled();
 
   for(auto timetable : timetables_){
-    if(timetable.size() >= max_scheduled){
+    if((int)timetable.size() >= max_scheduled){
       print_timetable(timetable);
     }
     
