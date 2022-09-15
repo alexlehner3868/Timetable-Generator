10580040939a93ef71cf3ca9648f9f2bb6b07ac0
Alex <angrybird3868@gmail.com>
2022-09-15 11:54:12 -0400

    Add notes

--- scheduler_front-end/src/Period.js
+++ scheduler_front-end/src/Period.js
@@ -2,6 +2,10 @@ import React from "react";
 
 
 const Period = (props) => {
+    // Props should give us classID (ECE421), SectionID (Lec01) and it should give us a classCountIdx (1),which will let us
+    //change the color of the period based on a color array
+    // TODO:
+    // Work out how to pass input
     const classData = new Array(2);
     classData[0] = (props.ClassID === "" ? null : props.ClassID);
     classData[1] = (props.SecionID === "" ? null : props.SecionID);
