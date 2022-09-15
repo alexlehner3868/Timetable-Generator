cc8d72c4cd27467db37d2c38a823f98dd2fb0a78
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-09-15 19:24:30 -0400

    Started sidebar addition

--- scheduler_front-end/src/Timetable.js
+++ scheduler_front-end/src/Timetable.js
@@ -2,6 +2,11 @@ import React from "react";
 import Period from "./Period";
 
 const Timetable = () =>{
+    //make sidebar for seaching classes, adding and removing
+    //const sidebar = new Array(2);
+    //sidebar[0] = new Array(3).fill(null);
+    //sidebar[1] = new Array(1).fill(null);
+
     const timetable = new Array(10);
     for(let i = 0; i < timetable.length; i++){
         timetable[i] = new Array(5).fill(null);
@@ -18,6 +23,7 @@ const Timetable = () =>{
             )
         })}
         </div>
+
     )
 }
 
