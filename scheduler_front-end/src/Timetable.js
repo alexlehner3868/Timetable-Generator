131df556594731ff7ea86a97f1586b6147be131e
Alex <angrybird3868@gmail.com>
2022-09-16 10:52:17 -0400

    Little change to Timetable

--- scheduler_front-end/src/Timetable.js
+++ scheduler_front-end/src/Timetable.js
@@ -4,15 +4,15 @@ import Period from "./Period";
 //TODO:
 // Currently only printing for one semester. Need to do both semesters. 
 // Decide how to view the 2 semesters
-const Timetable = () =>{
+const Timetable = (props) =>{
     //make sidebar for seaching classes, adding and removing
     //const sidebar = new Array(2);
     //sidebar[0] = new Array(3).fill(null);
     //sidebar[1] = new Array(1).fill(null);
 
-    const timetable = new Array(10);
+    const timetable = new Array(10); 
     for(let i = 0; i < timetable.length; i++){
-        timetable[i] = new Array(5).fill(null);
+        timetable[i] = new Array(5).fill(null); 
     }
 
     return (
