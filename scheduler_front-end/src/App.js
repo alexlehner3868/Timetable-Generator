e1f88c8f476322f69fecb056fe5d6d7e278572de
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-09-16 16:01:59 -0400

    Updated TODO

--- scheduler_front-end/src/App.js
+++ scheduler_front-end/src/App.js
@@ -18,7 +18,15 @@ function App() {
   );
 }
 */
-
+// TODO: Get course data with class names
+// TODO: Properly space timetable and sidebar so they're on left and right side
+// TODO: When searching for a class, each result should be a button
+// TODO: Clicking on Class button shows LEC/TUT/PRA Info 
+// TODO: Clicking on Class button shows Add Course button
+// TODO: Section to display classes already added to the plan
+// TODO: Button to show only fall schedule
+// TODO: Button to show only winter schedule
+// TODO: Button to show both schedules
 function App() {
   return (
     <div className="App">
