3d24dfcad2036cc03a47b87f9799526db702d11c
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-10-08 14:40:33 -0400

    Reorganized main function

---
+++ algo/include/test.hh
@@ -0,0 +1,11 @@
+#ifndef TEST_H
+#define TEST_H
+
+using namespace std;
+
+unordered_set<CourseOfferings, CourseOfferings::CourseOfferingHash> get_classes();
+void test_sample_questions();
+void remove_course(unordered_set<CourseOfferings, CourseOfferings::CourseOfferingHash>& offerings, string course_id);
+
+
+#endif
