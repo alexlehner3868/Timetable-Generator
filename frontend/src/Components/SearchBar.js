c90a3fc518f523e98752e307643f4535b25c6a39
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2023-01-08 17:39:20 -0500

    Added tabs to sidebar

---
+++ frontend/src/Components/SearchBar.js
@@ -0,0 +1,58 @@
+// -- React --
+import React from "react";
+import {useState} from "react";
+
+// -- Components --
+import EditCourse from './EditCourse';
+
+// -- Data --
+import Data from "../data/courses.json";
+
+// -- Module --
+const SearchBar = ({courses, setCourses}) => {
+  const [query, setQuery]  = useState("");
+
+  return (
+    <div className="sidebar">
+      <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search For Class"
+        className="search-bar"
+        onChange={event => setQuery(event.target.value)}
+      />
+      <i className="fa fa-magnifying-glass fa-lg"></i>
+      </div>
+
+      <div className="search-results-box">
+        {
+          Data.filter(class_result => {
+            if (query === "") {
+              //nothing in the search bar, don't show anything
+              return class_result;
+            } else if (class_result.ACAD_ACT_CD.toLowerCase().includes(query.toLowerCase())) {
+              //returns array with all matches
+              return class_result;
+            } // TODO: add searching by class name here once we add class-Name to the database
+            return false;
+          }).map((class_result, index) => (
+            <div className="search-results" key={index}>
+              <div className="add-course-square" id="add-course-square" >
+                <h1 className="course-code"> {class_result.ACAD_ACT_CD} </h1>
+                <EditCourse
+                  action="{{ url_for('send-request') }}"
+                  method="post"
+                  course={class_result.ACAD_ACT_CD}
+                  setCourses={setCourses}
+                />
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+// -- Exports --
+export default SearchBar;
--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,57 +1,46 @@
 // -- React --
-import React from "react";
-import {useState} from "react";
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 // -- Components --
-import EditCourse from './Components/EditCourse';
-
-// -- Data --
-import Data from "./data/courses.json";
+import SearchBar from './Components/SearchBar';
 
 // -- Module --
-const Sidebar = ({courses, setCourses}) => {
-  const [query, setQuery]  = useState("");
+class Sidebar extends React.Component {
+  constructor() {
+    super();
+    this.state = { tabIndex: 0, message: ""};
+  }
 
-  return (
-    <div className="sidebar">
-      <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search For Class"
-        className="search-bar"
-        onChange={event => setQuery(event.target.value)}
-      />
-      <i className="fa fa-magnifying-glass fa-lg"></i>
-      </div>
+  render() {
+    const fall = {
+      day: { begin: 0, end: 5},
+      hour: { begin: 9, end: 21},
+    };
+    const winter = {
+      day: { begin: 5, end: 10},
+      hour: { begin: 9, end: 21},
+    };
 
-      <div className="search-results-box">
-        {
-          Data.filter(class_result => {
-            if (query === "") {
-              //nothing in the search bar, don't show anything
-              return class_result;
-            } else if (class_result.ACAD_ACT_CD.toLowerCase().includes(query.toLowerCase())) {
-              //returns array with all matches
-              return class_result;
-            } // TODO: add searching by class name here once we add class-Name to the database
-            return false;
-          }).map((class_result, index) => (
-            <div className="search-results" key={index}>
-              <div className="add-course-square" id="add-course-square" >
-                <h1 className="course-code"> {class_result.ACAD_ACT_CD} </h1>
-                <EditCourse
-                  action="{{ url_for('send-request') }}"
-                  method="post"
-                  course={class_result.ACAD_ACT_CD}
-                  setCourses={setCourses}
-                />
-              </div>
-            </div>
-          ))
-        }
-      </div>
-    </div>
-  )
+    return (
+      <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
+        <TabList>
+          <Tab>Search</Tab>
+          <Tab>Constraints</Tab>
+          <Tab>Classes</Tab>
+        </TabList>
+        <TabPanel>
+          <SearchBar courses={this.props.courses} setCourses={this.props.setCourses}/>
+        </TabPanel>
+        <TabPanel>
+          
+        </TabPanel>
+        <TabPanel>
+          
+        </TabPanel>
+      </Tabs>
+    );
+  }
 }
 
 // -- Exports --
