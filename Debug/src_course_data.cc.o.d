2fc74aa3b88a68f51db8ce6e5d5c67490441e398
Zakhary Kaplan <zakhary.kaplan@utoronto.ca>
2023-01-09 18:18:18 -0500

    chore: remove artifacts

--- Debug/src_course_data.cc.o.d
+++
@@ -1,4 +0,0 @@
-Debug/src_course_data.cc.o: src/course_data.cc course_data.hh \
- scheduler.hh
-course_data.hh:
-scheduler.hh:
--- build/dep/course_data.d
+++
@@ -1,4 +0,0 @@
-build/obj/course_data.o: src/course_data.cc dep/include/sqlite3.h \
- include/course_data.hh include/section.hh include/scheduler.hh \
- include/course_offering.hh include/constraints.hh include/course_data.hh \
- include/date.hh include/period.hh
