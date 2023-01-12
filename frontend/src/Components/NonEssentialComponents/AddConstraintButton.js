743f382420653d176ecea13c6da26bfc04bf8844
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2023-01-11 21:12:19 -0500

    Added constraint buttons (only aesthetic)

--- frontend/src/Components/NonEssentialComponents/AddConstraintButton.js
+++ frontend/src/Components/NonEssentialComponents/AddConstraintButton.js
@@ -45,40 +45,70 @@ const AddConstraintButton = (props) => {
   return (
        <div>
             <label>Prefer Morning Classes</label>
-            <select list="Priority" id="evening_priorities">
-                <option value="0">Select:</option>
+            <select list="Priority" id="morning_priorities">
+                <option value="0"></option>
                 <option value="1">Good To Have</option>
                 <option value="2">Great To Have</option>
                 <option value="3">Must Have</option>
             </select> <br></br>
             <label>Prefer Afternoon Classes</label>
             <select list="Priority" id="afternoon_priorities">
-                <option value="0">Select:</option>
+                <option value="0"></option>
                 <option value="1">Good To Have</option>
                 <option value="2">Great To Have</option>
                 <option value="3">Must Have</option>
             </select> <br></br>
             <label>Prefer Evening Classes</label>
             <select list="Priority" id="evening_priorities">
-                <option value="0">Select:</option>
+                <option value="0"></option>
                 <option value="1">Good To Have</option>
                 <option value="2">Great To Have</option>
                 <option value="3">Must Have</option>
             </select> <br></br>
-                <input type="checkbox" id="early" name="fav_language" value="early" onClick = {handleSubmitEarly}></input>
-                <label for="early">Prefer Morning Classes</label><br></br>
-                <input type="checkbox" id="afternoon" name="fav_language" value="afternoon" onClick = {handleSubmitAfternoon}></input>
-                <label for="afternoon">Prefer Afternoon Classes</label><br></br>
-                <input type="checkbox" id="evening" name="fav_language" value="evening" onClick = {handleSubmitEvening}></input>
-                <label for="evening">Prefer Evening Classes</label><br></br>
-                <input type="checkbox" id="min_day" name="fav_language" value="min_day" onClick = {handleSubmitMinDay}></input>
-                <label for="min_day">Prefer Minimum Days on Campus</label><br></br>
+            <label>Minimize Days with Classes</label>
+            <select list="Priority" id="minimize_priorities">
+                <option value="0"></option>
+                <option value="1">Good To Have</option>
+                <option value="2">Great To Have</option>
+                <option value="3">Must Have</option>
+            </select> <br></br>
+            <label>Prefer Async Classes</label>
+            <select list="Priority" id="async_priorities">
+                <option value="0"></option>
+                <option value="1">Good To Have</option>
+                <option value="2">Great To Have</option>
+                <option value="3">Must Have</option>
+            </select> <br></br>
+            <label>Prefer Sync Classes</label>
+            <select list="Priority" id="sync_priorities">
+                <option value="0"></option>
+                <option value="1">Good To Have</option>
+                <option value="2">Great To Have</option>
+                <option value="3">Must Have</option>
+            </select> <br></br>
+            <label>Prefer Lunch Break</label>
+            <select list="Priority" id="lunch_priorities">
+                <option value="0"></option>
+                <option value="1">Good To Have</option>
+                <option value="2">Great To Have</option>
+                <option value="3">Must Have</option>
+            </select> <br></br>
+            <label>Prefer Dinner Break</label>
+            <select list="Priority" id="dinner_priorities">
+                <option value="0"></option>
+                <option value="1">Good To Have</option>
+                <option value="2">Great To Have</option>
+                <option value="3">Must Have</option>
+            </select> <br></br>
+                
                 <label for="before">No Class Before: </label>
                 <input type="number" id="before" min="10" max="20" onClick = {handleSubmitBackBefore} ></input><br></br>
                 <label for="after">No Class After: </label>
                 <input type="number" id="after" min="9" max="19" onClick = {handleSubmitBackAfter} ></input><br></br>
-                <label for="max">Max Hours of Class Back to Back: </label>
-                <input type="number" id="max" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
+                <label for="max">Max Hours of Class Back to Back</label>
+                <input type="number" id="max_class" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
+                <label for="max">Max Hours of Break</label>
+                <input type="number" id="max_break" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
 
                 <fieldset class="fieldset-form">
                         <legend> Block Off Time </legend>
