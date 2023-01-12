7842eadeea9f2c0ce8b656c919e16906b5514412
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2023-01-12 13:58:01 -0500

    Dropdowns should be mostly functional

--- frontend/src/Components/NonEssentialComponents/AddConstraintButton.js
+++ frontend/src/Components/NonEssentialComponents/AddConstraintButton.js
@@ -11,14 +11,32 @@ const AddConstraintButton = (props) => {
       console.log(`removing constraint: ${constraint}`)
       props.setConstraints(prev => new Set([...prev].filter(x => x !== constraint)))
     };
+    
+    const priority = 0;
+    /*
+        00 - prefer early classes
+        01 - prefer afternoon classes
+        02 - prefer evening classes
+        03 - prefer minimum days on campus 
+        04 - prefer async classes 
+        05 - prefer syunc classes
+        06 - prefer break at lunch
+        07 - prefer break at dinner
+        08 - no class before a certain time
+        09 - no class after a certain time
+        10 - max hours of class back to back
+        11 - no breaks longer than a certain time
+
+    */
 
     function handleSubmitEarly (event) {
+        
       event.preventDefault()
-      //these only work if they're all the same
-      addConstraint("")
+      console.log(document.getElementById("morning_priorities").value);
       console.log("Prioritizing Early Classes")
     } function handleSubmitAfternoon (event) {
         event.preventDefault()
+        document.getElementById("afternoon_priorities").value = priority;
         addConstraint()
         console.log("Prioritizing Afternoon Classes")
     } function handleSubmitEvening (event) {
@@ -40,16 +58,21 @@ const AddConstraintButton = (props) => {
         addConstraint()
         console.log("Blocking Off Time")
     }
-    //use fieldset disabled key for constraints later
+    
+    
+    
   return (
        <div>
             <label>Prefer Morning Classes</label>
-            <select list="Priority" id="morning_priorities">
-                <option value="0"></option>
-                <option value="1">Good To Have</option>
-                <option value="2">Great To Have</option>
-                <option value="3">Must Have</option>
+            <select list="Priority" id="morning_priorities" onClick = {handleSubmitEarly}>
+                <option value="0" ></option>
+                <option value="1" >Good To Have</option>
+                <option value="2" >Great To Have</option>
+                <option value="3" >Must Have</option>
             </select> <br></br>
+
+
+
             <label>Prefer Afternoon Classes</label>
             <select list="Priority" id="afternoon_priorities">
                 <option value="0"></option>
@@ -100,13 +123,13 @@ const AddConstraintButton = (props) => {
                 <option value="3">Must Have</option>
             </select> <br></br>
                 
-            <label for="before">No Class Before: </label>
+            <label id="before">No Class Before: </label>
             <input type="number" id="before" min="10" max="20" onClick = {handleSubmitBackBefore} ></input><br></br>
-            <label for="after">No Class After: </label>
+            <label id="after">No Class After: </label>
             <input type="number" id="after" min="9" max="19" onClick = {handleSubmitBackAfter} ></input><br></br>
-            <label for="max">Max Hours of Class Back to Back</label>
+            <label id="max">Max Hours of Class Back to Back</label>
             <input type="number" id="max_class" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
-            <label for="max">Max Hours of Break</label>
+            <label id="max">Max Hours of Break</label>
             <input type="number" id="max_break" min="0" max="12" onClick = {handleSubmitBreak} ></input><br></br>
 
        </div>
