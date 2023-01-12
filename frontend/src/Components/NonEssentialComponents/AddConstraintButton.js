d954412663a3d4af560bc584fc9aeb5251e9b811
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2023-01-11 21:20:09 -0500

    Removed useless html

--- frontend/src/Components/NonEssentialComponents/AddConstraintButton.js
+++ frontend/src/Components/NonEssentialComponents/AddConstraintButton.js
@@ -34,9 +34,8 @@ const AddConstraintButton = (props) => {
         addConstraint()
         console.log("Prioritize Classes After X")
     } function handleSubmitBackAfter (event) {
-
     } function handleSubmitBack (event) {
-    } function handleSubmitBlock (event) {
+    } function handleSubmitBreak (event) {
         event.preventDefault()
         addConstraint()
         console.log("Blocking Off Time")
@@ -101,62 +100,14 @@ const AddConstraintButton = (props) => {
                 <option value="3">Must Have</option>
             </select> <br></br>
                 
-                <label for="before">No Class Before: </label>
-                <input type="number" id="before" min="10" max="20" onClick = {handleSubmitBackBefore} ></input><br></br>
-                <label for="after">No Class After: </label>
-                <input type="number" id="after" min="9" max="19" onClick = {handleSubmitBackAfter} ></input><br></br>
-                <label for="max">Max Hours of Class Back to Back</label>
-                <input type="number" id="max_class" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
-                <label for="max">Max Hours of Break</label>
-                <input type="number" id="max_break" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
-
-                <fieldset class="fieldset-form">
-                        <legend> Block Off Time </legend>
-                        <form>
-                            <label for="block">Block off: </label>
-                            <select list="time" id="times">
-                                <option value="none">Select:</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                            </select>
-                            <label for="block"> Hours on: </label>
-                            <select list="day" id="days">
-                                <option value="none">Select:</option>
-                                <option value="Monday">Monday</option>
-                                <option value="Tuesday">Tuesday</option>
-                                <option value="Wednesday">Wednesday</option>
-                                <option value="Thursday">Thursday</option>
-                                <option value="Friday">Friday</option>
-                            </select>
-                            <label for="block"> at: </label>
-                            <select list="time_" id="times_">
-                                <option value="none">Select:</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">1</option>
-                                <option value="14">2</option>
-                                <option value="15">3</option>
-                                <option value="16">4</option>
-                                <option value="17">5</option>
-                                <option value="18">6</option>
-                                <option value="19">7</option>
-                                <option value="20">8</option>
-                            </select>
-                            <input type="submit" onClick = {handleSubmitBlock} ></input>
-                        </form>
-                </fieldset>
+            <label for="before">No Class Before: </label>
+            <input type="number" id="before" min="10" max="20" onClick = {handleSubmitBackBefore} ></input><br></br>
+            <label for="after">No Class After: </label>
+            <input type="number" id="after" min="9" max="19" onClick = {handleSubmitBackAfter} ></input><br></br>
+            <label for="max">Max Hours of Class Back to Back</label>
+            <input type="number" id="max_class" min="0" max="12" onClick = {handleSubmitBack} ></input><br></br>
+            <label for="max">Max Hours of Break</label>
+            <input type="number" id="max_break" min="0" max="12" onClick = {handleSubmitBreak} ></input><br></br>
 
        </div>
   )}
