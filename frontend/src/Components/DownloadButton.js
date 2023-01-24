abdc9aded6bec7065211cd9a3fcd3e1ddf3b5895
Alex <angrybird3868@gmail.com>
2023-01-24 14:30:13 -0500

    Roughly print list of one semester courses to pdf

--- frontend/src/Components/DownloadButton.js
+++ frontend/src/Components/DownloadButton.js
@@ -3,57 +3,47 @@ import { jsPDF } from "jspdf";
 // -- Module --
 const DownloadButton = (props) => {
   const timetable = (props.timetables.length) ? props.timetables[props.ttbIndex] : [];
-    const downloadTxtFile = () => {
-           // text content
-            
-            // Extract sorted list of courses (for colouring)
-            let courses = []; 
-            for (const meet of timetable){
-                if (!courses.includes(
-                    {
-                        "code":meet.course,
-                        "type":meet.type,
-                        "section" :meet.section
+    
+  function getTimetableCourseTxt (){
+    // Extract sorted list of courses (for colouring)
+    let courses = []; 
+    for (const meet of timetable){
+        if (!courses.includes(
+            {
+                "code":meet.course,
+                "type":meet.type,
+                "section" :meet.section
 
-                    })){
-                        courses.push({"code":meet.course,"type":meet.type, "section" :meet.section})
-                }
-            }
-            let text = [];
-
-            text.push("Timetable Option " + props.ttbIndex +":\n")
-            text.push("_________________________________________\n")
-            text.push("Here are the sections to enroll in:\n")
-            for( const course  of courses){
-                text.push("     " + course.code + " " + course.type + " section: " + course.section + '\n');
-            }
-            let unique_courses = [...new Set(text)]
-            console.log(unique_courses)
-            // file object
-            const file = new Blob(unique_courses, {type: 'text/plain'});
-            // anchor link
-            const element = document.createElement("a");
-            element.href = URL.createObjectURL(file);
-            element.download = "timetable_option_"+ props.ttbIndex + ".txt";
-            // simulate link click
-            document.body.appendChild(element);
-            // Required for this to work in FireFox
-            element.click();
+            })){
+                courses.push({"code":meet.course,"type":meet.type, "section" :meet.section})
+        }
     }
+    let text = "";
 
+    text += ("Timetable Option " + props.ttbIndex +":\n")
+    text +=("_________________________________________\n")
+    text +=("Here are the sections to enroll in:\n")
+    for( const course  of courses){
+        text += ("     " + course.code + " " + course.type + " section: " + course.section + '\n');
+    }
+    
+    return text;
+  }
+  
     function downloadPDFWithjsPDF() {
       const doc = new jsPDF('l', 'pt');
       doc.setFontSize(500);
-      
+      let course_txt = getTimetableCourseTxt();
       doc.html(document.querySelector('#timetable'), {
         callback: function (doc) {
-          
+          doc.addPage('l', 'pt');
+          doc.text(course_txt, '20', '20');
           doc.save("TimetableOption"+ (props.ttbIndex+1) + ".pdf");
         },
         margin: [30, 30, 30, 30],
         width: 552,
-      });
-      }
+      });     
+    }
 
 
     return (
