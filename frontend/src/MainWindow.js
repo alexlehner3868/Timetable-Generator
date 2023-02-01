bbb596b8f2826c23735a7692c72d90f3644a13b1
Alex <angrybird3868@gmail.com>
2023-02-01 18:38:59 -0500

    Count courses per semester

--- frontend/src/MainWindow.js
+++ frontend/src/MainWindow.js
@@ -5,6 +5,8 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // -- Components --
 import Semester from './Components/Semester';
 
+
+
 // -- Module --
 class MainWindow extends React.Component {
   constructor() {
@@ -12,7 +14,9 @@ class MainWindow extends React.Component {
     this.state = { tabIndex: 0, message: ""};
   }
 
+ 
   render() {
+
     const fall = {
       day: { begin: 0, end: 5},
       hour: { begin: 9, end: 21},
@@ -21,10 +25,21 @@ class MainWindow extends React.Component {
       day: { begin: 5, end: 10},
       hour: { begin: 9, end: 21},
     };
-
+    let fall_count = 0;
+    let winter_count = 0; 
     // Extract the timetable to show
     const timetable = (this.props.timetables.length) ? this.props.timetables[this.props.ttbIndex] : [];
-
+    for (const meet of timetable){
+      console.log(meet.day)
+      if(meet.day < 6){
+        fall_count++;
+      }else if( meet.day < 10){
+        winter_count++;
+        console.log("winter calss:",meet)
+      }
+    }
+    console.log("winter", winter_count)
+    
     return (
       <div className='main-window'>
         
